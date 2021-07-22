03298a098feba2186735e42905a175856b5bef4c
T449 <74192450+T-449@users.noreply.github.com>
2021-07-22 00:06:58 -0700

    Delete openair-epc-fed directory

--- openair-epc-fed/buildSPGWU.sh
+++
@@ -1,4 +0,0 @@
-sudo docker build --target oai-spgwu-tiny --tag oai-spgwu-tiny:production --file component/oai-spgwu-tiny/docker/Dockerfile.ubuntu18.04 component/oai-spgwu-tiny
-sudo docker image prune --force
-sudo docker image ls
-
--- openair-epc-fed/component/oai-mme/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# RELEASE NOTES: #
-
-## v1.1.0 -- September 2020 ##
-
-* Change the License Model to 3-Clause BSD
