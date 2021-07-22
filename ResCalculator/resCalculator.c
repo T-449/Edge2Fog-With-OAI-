b81cd9d3bd6e2cde10eccc0050d75d6563277f85
Tushin Mallick <purgatory@Tartarus.local>
2021-07-22 13:10:06 +0600

    No rerun required

--- ResCalculator/resCalculator.c
+++ ResCalculator/resCalculator.c
@@ -77,71 +77,6 @@ void tokenizehexString(char *hexString, char output[][32])
     }
 }
 
-int startServer(uint8_t *RAND_Octet)
-{
-    int socket_desc , client_sock , c , read_size;
-	struct sockaddr_in server , client;
-	char client_message[32];
-	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1)
-	{
-		printf("Could not create socket");
-	}
-	puts("Socket created");
-	
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server.sin_port = htons( 10500 );
-
-    if (setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
-        perror("setsockopt(SO_REUSEADDR) failed");
-	
-	//Bind
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		//print the error message
-		perror("bind failed. Error");
-		return 1;
-	}
-	puts("bind done");
-	
-	//Listen
-	listen(socket_desc , 3);
-	
-	//Accept and incoming connection
-	puts("Waiting for incoming connections...");
-	c = sizeof(struct sockaddr_in);
-	
-	//accept connection from an incoming client
-	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-	if (client_sock < 0)
-	{
-		perror("accept failed");
-		return 1;
-	}
-	puts("Connection accepted");
-	
-	//Receive a message from client
-	if(read_size = recv(client_sock , client_message , sizeof client_message , 0) > 0 )
-	{
-        char *AUTN_RAND = client_message;
-        hextoOctet(RAND_Octet, AUTN_RAND, 32);
-	}
-	
-	if(read_size == 0)
-	{
-		puts("Client disconnected");
-		fflush(stdout);
-	}
-	else if(read_size == -1)
-	{
-		perror("recv failed");
-	}
-}
-
 int startClient(char *response, int size)
 {
     int sock;
@@ -179,39 +114,100 @@ int startClient(char *response, int size)
 	return 0;
 }
 
-
-int main()
+int startServer(uint8_t *RAND_Octet)
 {
-    uint8_t K_Octet[strlen(USIM_API_K_VALUE)/2];
-    uint8_t RAND_Octet[16];
-    uint8_t RES_Octet[8];
-    uint8_t OP_Octet[16];
-    hextoOctet(K_Octet, USIM_API_K_VALUE, strlen(USIM_API_K_VALUE));
-    print(K_Octet, strlen(USIM_API_K_VALUE));
-    hextoOctet(OP_Octet, OPC, strlen(OPC));
-    print(OP_Octet, strlen(OPC));
-    startServer(RAND_Octet);
-    print(RAND_Octet, 32);
-    //char hexstring[] = "713858544ee54a36703e7937564fb550";
-    //hextoOctet(RAND_Octet, hexstring, 32);
-    f2345(K_Octet, RAND_Octet, RES_Octet, OP_Octet);
-    print(RES_Octet, 16);
-    char RES_str[17];
-    emptyArr(RES_str, 17);
-    int k;
-    for(k=0; k<8; k++)
-        sprintf(&RES_str[strlen(RES_str)], "%02x", RES_Octet[k]);
-    printf("%s\n", RES_str);
-    startClient(RES_str, 16);
+    int socket_desc , client_sock , c , read_size;
+	struct sockaddr_in server , client;
+	char client_message[32];
+	
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+	
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_port = htons( 10500 );
+
+    if (setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+        perror("setsockopt(SO_REUSEADDR) failed");
+	
+	//Bind
+	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		//print the error message
+		perror("bind failed. Error");
+		return 1;
+	}
+	puts("bind done");
+	
+	//Listen
+	listen(socket_desc , 3);
+	
+	//Accept and incoming connection
+	c = sizeof(struct sockaddr_in);
+	
+	//accept connection from an incoming client
+    while (1)
+    {
+        puts("Waiting for incoming connections...");
+        client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+        if (client_sock < 0)
+        {
+            perror("accept failed");
+            return 1;
+        }
+        puts("Connection accepted");
+        
+        //Receive a message from client
+        while (1)
+        {
+            if((read_size = recv(client_sock , client_message , sizeof client_message , 0)) > 0 )
+            {
+                char *AUTN_RAND = client_message;
+                hextoOctet(RAND_Octet, AUTN_RAND, 32);
+                print(RAND_Octet, 32);
+                uint8_t K_Octet[strlen(USIM_API_K_VALUE)/2];
+                uint8_t RES_Octet[8];
+                uint8_t OP_Octet[16];
+                hextoOctet(K_Octet, USIM_API_K_VALUE, strlen(USIM_API_K_VALUE));
+                //print(K_Octet, strlen(USIM_API_K_VALUE));
+                hextoOctet(OP_Octet, OPC, strlen(OPC));
+                //print(OP_Octet, strlen(OPC));
+                f2345(K_Octet, RAND_Octet, RES_Octet, OP_Octet);
+                print(RES_Octet, 16);
+                char RES_str[17];
+                emptyArr(RES_str, 17);
+                int k;
+                for(k=0; k<8; k++)
+                    sprintf(&RES_str[strlen(RES_str)], "%02x", RES_Octet[k]);
+                printf("%s\n", RES_str);
+                startClient(RES_str, 16);
+            }
+            
+            if(read_size == 0)
+            {
+                puts("Client disconnected");
+                fflush(stdout);
+                break;
+            }
+            else if(read_size == -1)
+            {
+                perror("recv failed");
+                break;
+            }
+        }
+    }
 }
 
 
-/*int k;
-    char k_str[hexStringSize+1];
-    emptyArr(k_str, hexStringSize+1);
-    for(k=0; k<(hexStringSize/2);k++)
-    {
-        sprintf(&k_str[strlen(k_str)], "%02x", octetArray[k]);
-    }
+int main()
+{
+    uint8_t RAND_Octet[16];
+    startServer(RAND_Octet);
+}
 
-    printf("%s\n",k_str);*/
